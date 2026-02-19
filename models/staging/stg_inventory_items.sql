9beaeae865935b03f65b4f38363762c52bd75d35
milocarlos90@gmail.com <milocarlos90@gmail.com>
2026-02-19 09:41:51 +0000

    new staging file + sources description

---
+++ models/staging/stg_inventory_items.sql
@@ -0,0 +1,29 @@
+with source as (
+
+    select *
+    from {{ source('thelook_ecommerce', 'inventory_items') }}
+
+),
+
+renamed as (
+
+    select
+        cast(id as int64)                        as inventory_item_id,
+        cast(product_id as int64)                as product_id,
+        cast(created_at as timestamp)            as created_at,
+        cast(sold_at as timestamp)               as sold_at,
+        cast(cost as numeric)                    as cost
+        cast(product_category as string)         as product_category,
+        cast(product_name as string)             as product_name,
+        cast(product_name as string)             as product_brand,
+        cast(product_retail_price as numeric)    as product_retail_price,
+        cast(product_department as string)       as product_department,
+        cast(product_sku as string)              as product_sku,
+        cast(product_distribution_id as integer) as product_distribution_id,
+
+    from source
+
+)
+
+select *
+from renamed
